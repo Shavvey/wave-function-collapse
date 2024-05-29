6e13af01f00f48d5463f2acd02a37e80a5a88472
Cole Johnson <coledjohn@gmail.com>
2024-05-29 16:25:03 -0600

    creating getRandom method

--- src/TileSet.java
+++ src/TileSet.java
@@ -1,17 +1,22 @@
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Random;
 
 public class TileSet {
     private Tile[][] tiles;
     // implementing a comparator for the
-    private final Comparator<Tile> compare = (t1, t2) -> {
+    private final static Comparator<Tile> compare = (t1, t2) -> {
         int len1 = t1.entropy();
         int len2 = t2.entropy();
         // compare the number of possible options the two tiles still have
         // this is serves as the rudimentary measure of "entropy"
         return Integer.compare(len1, len2);
     };
+    // construct a random seed to pull random number for
+    // used in the get random tile method
+    private static final Random rand = new Random();
+
 
     // constructor to create the tile set
     private final int cellsPerRow;
@@ -26,6 +31,17 @@ public class TileSet {
                 tiles[i][j] = new Tile();
             }
         }
+    }
+
+    // get a random tile from the tile set
+    public Tile getRandom() {
+        int len = tiles.length;
+        // get indices into 2d array
+        int idx1 = rand.nextInt(len);
+        int idx2 = rand.nextInt(len);
+        // return the a tile randomly indexed from the array
+        return tiles[idx1][idx2];
+
     }
 
     public int getCellsPerRow() {
@@ -55,6 +71,7 @@ public class TileSet {
         for (Tile t : tileList) {
             System.out.println("Compare val: " + t.entropy());
         }
+        Tile tile = getRandom();
 
     }
 }
